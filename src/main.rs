01e4f8e9b854a01038496f7cb25a47fdecfa1c3c
Edward Toroshchyn <dev@hades.name>
2025-08-14 10:59:11 +0200

    dispatch to the correct solver

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 mod ecclient;
+mod quest1;
+mod quest2;
+mod quest3;
+mod quest4;
 mod quest5;
 mod types;
 
 use std::env;
 
 use clap::Parser;
+use types::Part;
+use types::PuzzleKey;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -18,6 +24,87 @@ struct Args {
     event: i16,
     quest: i8,
     part: i8,
+}
+
+fn get_solver(puzzle_key: &PuzzleKey) -> Box<dyn Fn(&str) -> String> {
+    match puzzle_key {
+        PuzzleKey {
+            event: 2024,
+            quest: 1,
+            part: Part::One,
+        } => Box::new(quest1::solve_part_1),
+        PuzzleKey {
+            event: 2024,
+            quest: 1,
+            part: Part::Two,
+        } => Box::new(quest1::solve_part_2),
+        PuzzleKey {
+            event: 2024,
+            quest: 1,
+            part: Part::Three,
+        } => Box::new(quest1::solve_part_3),
+        PuzzleKey {
+            event: 2024,
+            quest: 2,
+            part: Part::One,
+        } => Box::new(quest2::solve_part_1),
+        PuzzleKey {
+            event: 2024,
+            quest: 2,
+            part: Part::Two,
+        } => Box::new(quest2::solve_part_2),
+        PuzzleKey {
+            event: 2024,
+            quest: 2,
+            part: Part::Three,
+        } => Box::new(quest2::solve_part_3),
+        PuzzleKey {
+            event: 2024,
+            quest: 3,
+            part: Part::One,
+        } => Box::new(quest3::solve_part_1),
+        PuzzleKey {
+            event: 2024,
+            quest: 3,
+            part: Part::Two,
+        } => Box::new(quest3::solve_part_2),
+        PuzzleKey {
+            event: 2024,
+            quest: 3,
+            part: Part::Three,
+        } => Box::new(quest3::solve_part_3),
+        PuzzleKey {
+            event: 2024,
+            quest: 4,
+            part: Part::One,
+        } => Box::new(quest4::solve_part_1),
+        PuzzleKey {
+            event: 2024,
+            quest: 4,
+            part: Part::Two,
+        } => Box::new(quest4::solve_part_1),
+        PuzzleKey {
+            event: 2024,
+            quest: 4,
+            part: Part::Three,
+        } => Box::new(quest4::solve_part_3),
+        PuzzleKey {
+            event: 2024,
+            quest: 5,
+            part: Part::One,
+        } => Box::new(quest5::solve_part_1),
+        PuzzleKey {
+            event: 2024,
+            quest: 5,
+            part: Part::Two,
+        } => Box::new(quest5::solve_part_2),
+        PuzzleKey {
+            event: 2024,
+            quest: 5,
+            part: Part::Three,
+        } => Box::new(quest5::solve_part_3),
+        _ => panic!("solver not found for {:?}", puzzle_key),
+    }
 }
 
 fn main() {
@@ -34,7 +121,6 @@ fn main() {
     } else {
         panic!("you must specify the session cookie with --cookie or EC_COOKIE env variable");
     };
-    let client = ecclient::EcClient::new(cookie.as_str()).expect("creating EC client");
     let key = types::PuzzleKey {
         event: args.event,
         quest: args.quest,
@@ -45,6 +131,8 @@ fn main() {
             _ => panic!("part should be in [1,2,3], got {}", args.part),
         },
     };
+    let solver = get_solver(&key);
+    let client = ecclient::EcClient::new(cookie.as_str()).expect("creating EC client");
     log::info!(
         "solving Everybody Codes event {} quest {} part {}",
         args.event,
@@ -54,7 +142,9 @@ fn main() {
     log::info!("retrieving puzzle input...");
     match client.get_puzzle_input(&key) {
         Ok(input) => {
-            println!("puzzle input: {}", input);
+            log::info!("solving...");
+            let solution = solver(input.as_str());
+            log::info!("solution: {}", solution);
         }
         Err(e) => {
             log::error!("error retrieving puzzle input: {e:#?}");
--- src/types.rs
+++ src/types.rs
@@ -1,9 +1,11 @@
+#[derive(Debug)]
 pub enum Part {
     One,
     Two,
     Three,
 }
 
+#[derive(Debug)]
 pub struct PuzzleKey {
     pub event: i16,
     pub quest: i8,
