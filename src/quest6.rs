4d000eaa3e9e9ecc94b9bd03053b9b35d605fd38
Edward Toroshchyn <dev@hades.name>
2025-08-14 13:33:29 +0200

    solve quest 6 part 2

--- src/quest6.rs
+++ src/quest6.rs
@@ -64,7 +64,63 @@ pub fn solve_part_1(input: &str) -> String {
 }
 
 pub fn solve_part_2(input: &str) -> String {
-    "".to_string()
+    let nodes: Vec<(&str, Vec<&str>)> = input
+        .lines()
+        .map(|line| {
+            let mut splits = line.splitn(2, ":");
+            let node = splits.next().unwrap();
+            let edges: Vec<&str> = splits.next().unwrap().split(",").collect();
+            (node, edges)
+        })
+        .collect();
+    let mut graph: petgraph::Graph<&str, (), petgraph::Directed, usize> = Graph::default();
+    let mut node_to_idx = HashMap::new();
+    nodes.iter().for_each(|node| {
+        node_to_idx.insert(node.0, graph.add_node(node.0));
+    });
+    nodes.iter().for_each(|node| {
+        debug!("{:?}", node);
+        node.1.iter().for_each(|to_node| {
+            let to_node_idx = match *to_node {
+                "@" => graph.add_node("@"),
+                _ => match node_to_idx.get(to_node) {
+                    Some(idx) => *idx,
+                    None => {
+                        let idx = graph.add_node(to_node);
+                        node_to_idx.insert(to_node, idx);
+                        idx
+                    }
+                },
+            };
+            graph.add_edge(node_to_idx[node.0], to_node_idx, ());
+        });
+    });
+    let mut node_paths = HashMap::new();
+    node_paths.insert(node_to_idx["RR"], vec!["RR".to_string()]);
+    depth_first_search(&graph, [node_to_idx["RR"]], |event| {
+        if let TreeEdge(from, to) = event {
+            let mut path = node_paths[&from].clone();
+            path.push(graph[to].to_string());
+            node_paths.insert(to, path);
+        }
+    });
+    let unique_len = node_paths
+        .iter()
+        .filter(|p| p.1.last().unwrap() == "@")
+        .counts_by(|p| p.1.len())
+        .into_iter()
+        .filter(|(_, counts)| *counts == 1)
+        .exactly_one()
+        .unwrap()
+        .0;
+    node_paths
+        .into_values()
+        .filter(|p| p.last().unwrap() == "@" && p.len() == unique_len)
+        .exactly_one()
+        .unwrap()
+        .iter()
+        .map(|n| n.chars().next().unwrap())
+        .join("")
 }
 
 pub fn solve_part_3(input: &str) -> String {
@@ -98,10 +154,17 @@ H:@"
     #[test]
     fn test_solve_part_2() {
         assert_eq!(
-            "",
+            "RB@",
             solve_part_2(
-                "2 3 4 5
-6 7 8 9"
+                "RR:A,BB,C
+A:D,E
+BB:F,@
+C:G,H
+D:@
+E:@
+F:@
+G:@
+H:@"
             )
         );
     }
