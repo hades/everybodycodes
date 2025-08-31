2e90c301fd7bc4f51f58c3d22b2cd0d0db0716f6
Edward Toroshchyn <dev@hades.name>
2025-08-31 19:56:34 +0200

    solve quest 2

--- src/event1/quest2.rs
+++ src/event1/quest2.rs
@@ -1,18 +1,23 @@
+use std::mem::swap;
+
 use itertools::Itertools;
 use regex::Regex;
 
+#[derive(Debug)]
 struct Node {
+    id: i64,
     key: i64,
     symbol: char,
     left: Option<Box<Node>>,
     right: Option<Box<Node>>,
 }
 
+#[derive(Debug)]
 struct Tree {
     root: Option<Box<Node>>,
 }
 
-fn insert(tree: &mut Tree, key: i64, symbol: char) {
+fn insert(tree: &mut Tree, id: i64, key: i64, symbol: char) {
     let mut node = &mut tree.root;
     while let Some(next_node) = node {
         if next_node.key > key {
@@ -21,7 +26,67 @@ fn insert(tree: &mut Tree, key: i64, symbol: char) {
             node = &mut next_node.right;
         }
     }
-    *node = Some(Box::new(Node{key, symbol, left: None, right: None}));
+    *node = Some(Box::new(Node {
+        id,
+        key,
+        symbol,
+        left: None,
+        right: None,
+    }));
+}
+
+fn find_node_mut(tree: &mut Tree, id: i64) -> &mut Node {
+    let mut stack: Vec<_> = tree.root.iter_mut().map(|n| n.as_mut()).collect();
+    while let Some(node) = stack.pop() {
+        if node.id == id {
+            return node;
+        }
+        if let Some(ref mut next_node) = node.left {
+            stack.push(next_node.as_mut());
+        }
+        if let Some(ref mut next_node) = node.right {
+            stack.push(next_node.as_mut());
+        }
+    }
+    unreachable!()
+}
+
+fn swap_values(left: &mut Tree, right: &mut Tree, id: i64) {
+    let left_node = find_node_mut(left, id);
+    let right_node = find_node_mut(right, id);
+    (left_node.key, right_node.key) = (right_node.key, left_node.key);
+    (left_node.symbol, right_node.symbol) = (right_node.symbol, left_node.symbol);
+}
+
+fn find_nodes_get_parent(tree: &mut Option<Box<Node>>, id: i64) -> Vec<&mut Option<Box<Node>>> {
+    let mut stack: Vec<_> = vec![tree];
+    let mut res = vec![];
+    while let Some(node_holder) = stack.pop() {
+        if node_holder.is_none() {
+            continue;
+        }
+        let node_id = node_holder.as_ref().unwrap().id;
+        if node_id == id {
+            res.push(node_holder);
+            continue;
+        }
+        let node = node_holder.as_mut().unwrap();
+        stack.push(&mut node.left);
+        stack.push(&mut node.right);
+    }
+    res
+}
+
+fn swap_with_detach(left: &mut Tree, right: &mut Tree, id: i64) {
+    let mut left_holders = find_nodes_get_parent(&mut left.root, id);
+    let mut right_holders = find_nodes_get_parent(&mut right.root, id);
+    let (left, right) = match (left_holders.len(), right_holders.len()) {
+        (0, 2) => (right_holders.pop(), right_holders.pop()),
+        (1, 1) => (left_holders.pop(), right_holders.pop()),
+        (2, 0) => (left_holders.pop(), left_holders.pop()),
+        _ => unreachable!(),
+    };
+    swap(left.unwrap(), right.unwrap());
 }
 
 fn as_rank_refs(tree: &Tree) -> Vec<Vec<&Node>> {
@@ -46,23 +111,114 @@ fn as_rank_refs(tree: &Tree) -> Vec<Vec<&Node>> {
 }
 
 fn largest_rank_symbols(tree: &Tree) -> String {
-    let largest_rank = as_rank_refs(&tree).into_iter().max_by_key(|rank| rank.len()).unwrap();
-    largest_rank.into_iter().map(|node| node.symbol).join("")
+    let rank_refs = as_rank_refs(&tree);
+    let largest_rank_size = rank_refs.iter().map(|r| r.len()).max().unwrap();
+    rank_refs
+        .into_iter()
+        .filter(|r| r.len() == largest_rank_size)
+        .next()
+        .unwrap()
+        .into_iter()
+        .map(|node| node.symbol)
+        .join("")
 }
 
 pub fn solve_part_1(input: &str) -> String {
     let re = Regex::new(r"ADD id=([0-9]+) left=\[([0-9]+),(.)\] right=\[([0-9]+),(.)\]").unwrap();
     let mut left = Tree { root: None };
     let mut right = Tree { root: None };
-    for (_, [_node_id, left_key, left_symbol, right_key, right_symbol]) in re.captures_iter(input).map(|c| c.extract()) {
-        insert(&mut left, left_key.parse::<i64>().unwrap(), left_symbol.chars().next().unwrap());
-        insert(&mut right, right_key.parse::<i64>().unwrap(), right_symbol.chars().next().unwrap());
+    for (_, [_node_id, left_key, left_symbol, right_key, right_symbol]) in
+        re.captures_iter(input).map(|c| c.extract())
+    {
+        insert(
+            &mut left,
+            0,
+            left_key.parse::<i64>().unwrap(),
+            left_symbol.chars().next().unwrap(),
+        );
+        insert(
+            &mut right,
+            0,
+            right_key.parse::<i64>().unwrap(),
+            right_symbol.chars().next().unwrap(),
+        );
     }
-    format!("{}{}", largest_rank_symbols(&left), largest_rank_symbols(&right))
+    format!(
+        "{}{}",
+        largest_rank_symbols(&left),
+        largest_rank_symbols(&right)
+    )
 }
 
-pub fn solve_part_2(input: &str) -> String { unimplemented!()}
-pub fn solve_part_3(input: &str) -> String { unimplemented!()}
+pub fn solve_part_2(input: &str) -> String {
+    let add_re =
+        Regex::new(r"ADD id=([0-9]+) left=\[([0-9]+),(.)\] right=\[([0-9]+),(.)\]").unwrap();
+    let swap_re = Regex::new(r"SWAP ([0-9]+)").unwrap();
+    let mut left = Tree { root: None };
+    let mut right = Tree { root: None };
+    for line in input.lines() {
+        if let Some((_, [node_id, left_key, left_symbol, right_key, right_symbol])) =
+            add_re.captures(line).map(|c| c.extract())
+        {
+            insert(
+                &mut left,
+                node_id.parse::<i64>().unwrap(),
+                left_key.parse::<i64>().unwrap(),
+                left_symbol.chars().next().unwrap(),
+            );
+            insert(
+                &mut right,
+                node_id.parse::<i64>().unwrap(),
+                right_key.parse::<i64>().unwrap(),
+                right_symbol.chars().next().unwrap(),
+            );
+            continue;
+        }
+        if let Some((_, [node_id])) = swap_re.captures(line).map(|c| c.extract()) {
+            swap_values(&mut left, &mut right, node_id.parse::<i64>().unwrap());
+        }
+    }
+    format!(
+        "{}{}",
+        largest_rank_symbols(&left),
+        largest_rank_symbols(&right)
+    )
+}
+
+pub fn solve_part_3(input: &str) -> String {
+    let add_re =
+        Regex::new(r"ADD id=([0-9]+) left=\[([0-9]+),(.)\] right=\[([0-9]+),(.)\]").unwrap();
+    let swap_re = Regex::new(r"SWAP ([0-9]+)").unwrap();
+    let mut left = Tree { root: None };
+    let mut right = Tree { root: None };
+    for line in input.lines() {
+        if let Some((_, [node_id, left_key, left_symbol, right_key, right_symbol])) =
+            add_re.captures(line).map(|c| c.extract())
+        {
+            insert(
+                &mut left,
+                node_id.parse::<i64>().unwrap(),
+                left_key.parse::<i64>().unwrap(),
+                left_symbol.chars().next().unwrap(),
+            );
+            insert(
+                &mut right,
+                node_id.parse::<i64>().unwrap(),
+                right_key.parse::<i64>().unwrap(),
+                right_symbol.chars().next().unwrap(),
+            );
+            continue;
+        }
+        if let Some((_, [node_id])) = swap_re.captures(line).map(|c| c.extract()) {
+            swap_with_detach(&mut left, &mut right, node_id.parse::<i64>().unwrap());
+        }
+    }
+    format!(
+        "{}{}",
+        largest_rank_symbols(&left),
+        largest_rank_symbols(&right)
+    )
+}
 
 #[cfg(test)]
 mod tests {
@@ -71,14 +227,22 @@ mod tests {
 
     #[test]
     fn test_part_1() {
-        assert_eq!("CFGNLK", solve_part_1("ADD id=1 left=[10,A] right=[30,H]
+        assert_eq!(
+            "CFGNLK",
+            solve_part_1(
+                "ADD id=1 left=[10,A] right=[30,H]
 ADD id=2 left=[15,D] right=[25,I]
 ADD id=3 left=[12,F] right=[31,J]
 ADD id=4 left=[5,B] right=[27,L]
 ADD id=5 left=[3,C] right=[28,M]
 ADD id=6 left=[20,G] right=[32,K]
-ADD id=7 left=[4,E] right=[21,N]"));
-        assert_eq!("EVERYBODYCODES", solve_part_1("ADD id=1 left=[160,E] right=[175,S]
+ADD id=7 left=[4,E] right=[21,N]"
+            )
+        );
+        assert_eq!(
+            "EVERYBODYCODES",
+            solve_part_1(
+                "ADD id=1 left=[160,E] right=[175,S]
 ADD id=2 left=[140,W] right=[224,D]
 ADD id=3 left=[122,U] right=[203,F]
 ADD id=4 left=[204,N] right=[114,G]
@@ -97,6 +261,43 @@ ADD id=16 left=[252,D] right=[276,M]
 ADD id=17 left=[258,I] right=[245,P]
 ADD id=18 left=[117,O] right=[283,!]
 ADD id=19 left=[212,O] right=[127,R]
-ADD id=20 left=[278,A] right=[169,C]"));
+ADD id=20 left=[278,A] right=[169,C]"
+            )
+        );
+    }
+
+    #[test]
+    fn test_part_3() {
+        assert_eq!(
+            "DJMGL",
+            solve_part_3(
+                "ADD id=1 left=[10,A] right=[30,H]
+ADD id=2 left=[15,D] right=[25,I]
+ADD id=3 left=[12,F] right=[31,J]
+ADD id=4 left=[5,B] right=[27,L]
+ADD id=5 left=[3,C] right=[28,M]
+SWAP 1
+SWAP 5
+ADD id=6 left=[20,G] right=[32,K]
+ADD id=7 left=[4,E] right=[21,N]
+SWAP 2"
+            )
+        );
+        assert_eq!(
+            "DJCGL",
+            solve_part_3(
+                "ADD id=1 left=[10,A] right=[30,H]
+ADD id=2 left=[15,D] right=[25,I]
+ADD id=3 left=[12,F] right=[31,J]
+ADD id=4 left=[5,B] right=[27,L]
+ADD id=5 left=[3,C] right=[28,M]
+SWAP 1
+SWAP 5
+ADD id=6 left=[20,G] right=[32,K]
+ADD id=7 left=[4,E] right=[21,N]
+SWAP 2
+SWAP 5"
+            )
+        );
     }
 }
